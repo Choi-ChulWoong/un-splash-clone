67f6cf68f890f7489108859cd9c8dac81eedee7d
IAN CHOI <endnd28@gmail.com>
2023-11-27 23:47:35 +0900

    feat: layout 컴포넌트 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import type { AppProps } from 'next/app';
 import GlobalStyle from '@/styles/Global';
+import Layout from '@/components/layout/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
-    <>
+    <Layout>
         <GlobalStyle />
         <Component {...pageProps} />
-    </>
+    </Layout>
   );
 }
