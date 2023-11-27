ff31b60d46b83410d06087260040973552d3b5c1
IAN CHOI <endnd28@gmail.com>
2023-11-27 23:24:29 +0900

    style: 공용 스타일 추가

---
+++ src/styles/Common.ts
@@ -0,0 +1,14 @@
+export const Common = {
+  colors: {
+    red_50:'#f15151',
+    red_100:'#e04c4c',
+    green_50: '#3cb46e',
+    green_100: '#37a866',
+    gray_50: '#f5f5f5',
+    gray_100: '#eee',
+    black: '#000',
+  },
+  screen: {
+    width: 'min(100%, 1320px)',
+  },
+};
