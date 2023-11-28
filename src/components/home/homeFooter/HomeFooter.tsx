a45902e3271444d26d3cd9f760aabbbcabff7308
IAN CHOI <endnd28@gmail.com>
2023-11-29 00:56:14 +0900

    feat: HomeFooter 컴포넌트 추가

---
+++ src/components/home/homeFooter/HomeFooter.tsx
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+import { Common } from '@/styles/Common';
+
+const HomeFooterBase = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 130px;
+  border-top: 1px solid ${Common.colors.gray_100};
+  background-color: white;
+  z-index: 1000;
+`;
+
+function HomeFooter() {
+  return <HomeFooterBase>Un-splash Footer.</HomeFooterBase>;
+}
+
+export default HomeFooter;
