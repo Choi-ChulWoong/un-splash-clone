81234011b95fadf8781e7b15b8eb02d95d8651ca
IAN CHOI <endnd28@gmail.com>
2023-11-28 21:12:50 +0900

    feat: homeHeader 컴포넌트 추가
    - Home 페이지에 들어가는 헤더 컴포넌트 추가

---
+++ src/components/home/homeHeader/HomeHeader.styles.ts
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+
+export const HeaderBase = styled.header`
+  padding: 60px 20px;
+  display: flex;
+  gap: 24px;
+`;
+export const SearchContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+`;
+
+export const HeaderTitle = styled.h1`
+  font-size: 2.5rem;
+`;
+
+export const HeaderDescription = styled.p`
+  margin-top: 8px;
+  font-size: 1.2rem;
+  letter-spacing: 0.1rem;
+  line-height: 1.5;
+`;
