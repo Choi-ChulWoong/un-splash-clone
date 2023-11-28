75a19156c86dd0a4ae46a95641af3c86cadc44cc
IAN CHOI <endnd28@gmail.com>
2023-11-28 21:13:29 +0900

    style: 전역 스타일 변수에 색상 추가

--- src/styles/Common.ts
+++ src/styles/Common.ts
@@ -1,11 +1,14 @@
 export const Common = {
   colors: {
-    red_50:'#f15151',
-    red_100:'#e04c4c',
+    red_50: '#f15151',
+    red_100: '#e04c4c',
     green_50: '#3cb46e',
     green_100: '#37a866',
     gray_50: '#f5f5f5',
     gray_100: '#eee',
+    gray_200: '#d1d1d1',
+    gray_300: '#767676',
+    gray_400: '#5F5F5F',
     black: '#000',
   },
   screen: {
