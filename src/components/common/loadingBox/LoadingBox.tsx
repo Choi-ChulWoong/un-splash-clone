506032f86f5e29fc695cd53396b5280b2fd6c164
IAN CHOI <endnd28@gmail.com>
2023-11-29 23:33:51 +0900

    minor: 사용하지 않는 코드 삭제

--- src/components/common/loadingBox/LoadingBox.tsx
+++ src/components/common/loadingBox/LoadingBox.tsx
@@ -3,7 +3,6 @@ import * as S from '@/components/common/loadingBox/LoadingBox.styled';
 function LoadingBox() {
   return (
     <S.LoadingBoxBase>
-      ????
       <S.Spinner />
     </S.LoadingBoxBase>
   );
