3e43aff775b7b93627c78cc3b56035eb44e279ed
IAN CHOI <endnd28@gmail.com>
2023-11-29 22:50:06 +0900

    feat: networkErrorboundary 추가
    - 네트워크 관련 에러바운더리를 추가함

---
+++ src/components/error/networkError/NetworkError.tsx
@@ -0,0 +1,20 @@
+import ErrorBox from '@/components/error/errorBox/ErrorBox';
+import RetryButton from '@/components/common/retryButton/RetryButton';
+
+interface NetworkErrorProps {
+  onRetry: () => void;
+}
+function NetworkError({ onRetry }: NetworkErrorProps) {
+  return (
+    <ErrorBox>
+      <h3>현재 접속이 원활하지 않아요..</h3>
+      <p>일시적인 서버오류가 발생했습니다.</p>
+      <p>잠시 후 다시 시도해 주세요.</p>
+      <RetryButton type="button" onClick={onRetry}>
+        다시 시도하기
+      </RetryButton>
+    </ErrorBox>
+  );
+}
+
+export default NetworkError;
