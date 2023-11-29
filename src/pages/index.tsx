3d7623d1fe03d58ef69024d6e7fe4a0de6f31e6e
IAN CHOI <endnd28@gmail.com>
2023-11-29 22:52:54 +0900

    feat: home 페이지에 errorboundary, suspense 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
 import HomeHeader from '@/components/home/homeHeader/HomeHeader';
-import HomeSection from '@/components/home/homeSection/HomeSection';
+import HomeSection from '@/containers/home/homeSection/HomeSection';
 import HomeFooter from '@/components/home/homeFooter/HomeFooter';
+import NetworkErrorBoundary from '@/components/error/networkErrorBoundary/NetworkErrorBoundary';
+import LoadingBox from '@/components/common/loadingBox/LoadingBox';
+import { Suspense } from 'react';
 
 export default function Home() {
   return (
     <>
-      <HomeHeader/>
-      <HomeSection/>
-      <HomeFooter/>
+      <HomeHeader />
+      <NetworkErrorBoundary>
+        <Suspense fallback={<LoadingBox />}>
+          <HomeSection />
+        </Suspense>
+      </NetworkErrorBoundary>
+      <HomeFooter />
     </>
-  )
+  );
 }
