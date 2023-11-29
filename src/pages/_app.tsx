0819c9ee3ad901398b88747a66044785e48e8d19
IAN CHOI <endnd28@gmail.com>
2023-11-29 22:52:33 +0900

    feat: _app.tsx에 provider, erroboundary 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,29 @@
 import type { AppProps } from 'next/app';
 import GlobalStyle from '@/styles/Global';
 import Layout from '@/components/layout/Layout';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import GlobalErrorBoundary from '@/components/error/globalErrorBoundary/GlobalErrorBoundary';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: 1,
+        staleTime: 1000 * 20,
+      },
+    },
+  });
 
   return (
-    <Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
         <GlobalStyle />
-        <Component {...pageProps} />
-    </Layout>
+        <GlobalErrorBoundary>
+          <Component {...pageProps} />
+        </GlobalErrorBoundary>
+      </Layout>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
