565993af601acd25868d973f016437e86c31464b
IAN CHOI <endnd28@gmail.com>
2023-11-29 22:54:10 +0900

    feat: loadingBox 컴포넌트 추가

---
+++ src/components/common/loadingBox/LoadingBox.tsx
@@ -0,0 +1,12 @@
+import * as S from '@/components/common/loadingBox/LoadingBox.styled';
+
+function LoadingBox() {
+  return (
+    <S.LoadingBoxBase>
+      ????
+      <S.Spinner />
+    </S.LoadingBoxBase>
+  );
+}
+
+export default LoadingBox;
